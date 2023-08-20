4ba7ee41cae1c2f15d84a73963010634ad25aa35
uji100 <97239721+uji100@users.noreply.github.com>
2023-08-20 23:26:26 +0530

    update login screen

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/LoginController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/LoginController.java
@@ -1,6 +1,31 @@
 package software.ujithamigara.orm_concepts_course_work.Controller;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField userNameTxtField;
+
+    @FXML
+    void forgetPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void passwordCheckBoxOnAction(ActionEvent event) {
+
+    }
 }
