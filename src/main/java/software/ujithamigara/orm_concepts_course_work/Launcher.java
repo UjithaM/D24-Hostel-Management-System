04da8a074512f61037979022106ab3aee35156f5
uji100 <97239721+uji100@users.noreply.github.com>
2023-09-01 15:00:57 +0530

    update pom.xml with dependency

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Launcher.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Launcher.java
@@ -17,7 +17,7 @@ public class Launcher extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch();
     }
 }
