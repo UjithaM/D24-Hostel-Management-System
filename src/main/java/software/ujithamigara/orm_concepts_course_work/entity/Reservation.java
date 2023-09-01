c05dd3246565858bb565bbd97f702ff161d6591b
uji100 <97239721+uji100@users.noreply.github.com>
2023-09-01 15:11:25 +0530

    add and implement Reservation entity

---
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/entity/Reservation.java
@@ -0,0 +1,10 @@
+package software.ujithamigara.orm_concepts_course_work.entity;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private String reservationId;
+    private Date date;
+    private String status;
+}
