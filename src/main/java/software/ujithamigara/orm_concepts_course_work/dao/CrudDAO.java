682564b0341255c1055f12eb9b73dee8d7a90cb7
uji100 <ujithamigara@gmail.com>
2023-09-04 12:30:38 +0530

    add IO exception

--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/CrudDAO.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/CrudDAO.java
@@ -1,10 +1,11 @@
 package software.ujithamigara.orm_concepts_course_work.dao;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public interface CrudDAO<entity> {
-    boolean save(entity entity) throws SQLException;
-    boolean delete(String id) throws SQLException;
-    entity search(String id) throws SQLException ;
-    boolean update(entity entity) throws SQLException ;
+    boolean save(entity entity) throws SQLException, IOException;
+    boolean delete(String id) throws SQLException, IOException;
+    entity search(String id) throws SQLException, IOException ;
+    boolean update(entity entity) throws SQLException, IOException ;
 }
