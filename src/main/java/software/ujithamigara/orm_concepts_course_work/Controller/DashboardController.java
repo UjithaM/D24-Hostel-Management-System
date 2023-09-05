4586f2357a07c616363ce242be06fb7053f5ce37
uji100 <ujithamigara@gmail.com>
2023-09-05 20:26:44 +0530

    add all components and add all fx id to it

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/DashboardController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/DashboardController.java
@@ -1,4 +1,52 @@
 package software.ujithamigara.orm_concepts_course_work.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+
+    @FXML
+    private Label adminNameLabel;
+
+    @FXML
+    private Label availableRoomLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    private Label reserveRoomLabel;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Label totalRoomCountLabl;
+
+    @FXML
+    void dashboardButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reservationButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void roomButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signOutButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void studentButtonOnAction(ActionEvent event) {
+
+    }
 }
