6c3a7ac9efd974fe25edbf3f95fd55c24034c441
uji100 <ujithamigara@gmail.com>
2023-09-06 11:42:27 +0530

    change things in FactoryConfiguration

--- src/main/java/software/ujithamigara/orm_concepts_course_work/util/FactoryConfiguration.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/util/FactoryConfiguration.java
@@ -1,41 +1,47 @@
 package software.ujithamigara.orm_concepts_course_work.util;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
 import software.ujithamigara.orm_concepts_course_work.entity.Room;
 import software.ujithamigara.orm_concepts_course_work.entity.Student;
+import software.ujithamigara.orm_concepts_course_work.entity.User;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class FactoryConfiguration {
 
+public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
 
     private SessionFactory sessionFactory;
-    private  FactoryConfiguration() throws IOException {
 
+    private FactoryConfiguration() throws IOException {
+        //add configuration file
         Configuration configuration = new Configuration();
 
-        //get properties from file
-        FileInputStream fileInputStream = new FileInputStream("src/main/resources/hibernate.properties");
+        //add properties
         Properties properties = new Properties();
-        properties.load(fileInputStream);
 
-        //add properties to configuration
-        configuration.addProperties(properties);
-        configuration.addAnnotatedClass(Student.class);
+        //add already created hibernate file to properties in current thread
+        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+
+        //add properties to configure
+        configuration.setProperties(properties);
+
+        //add annotated class to configure
+        configuration.addAnnotatedClass(User.class);
         configuration.addAnnotatedClass(Room.class);
         configuration.addAnnotatedClass(Reservation.class);
+        configuration.addAnnotatedClass(Student.class);
 
         //build session factory
         sessionFactory = configuration.buildSessionFactory();
     }
 
     public static FactoryConfiguration getInstance() throws IOException {
-        return (factoryConfiguration==null)? factoryConfiguration=new FactoryConfiguration() : factoryConfiguration;
+        return (factoryConfiguration==null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
     }
 
     public Session getSession(){
