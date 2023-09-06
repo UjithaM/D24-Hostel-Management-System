bbfb3c37d3939227766b573ec832f230a0212665
uji100 <ujithamigara@gmail.com>
2023-09-06 22:20:22 +0530

    add ReservationDAO and ReservationDAOImpl

---
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/ReservationDAO.java
@@ -0,0 +1,7 @@
+package software.ujithamigara.orm_concepts_course_work.dao.custom;
+
+import software.ujithamigara.orm_concepts_course_work.dao.CrudDAO;
+import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
+
+public interface ReservationDAO extends CrudDAO<Reservation> {
+}
---
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/ReservationDAOImpl.java
@@ -0,0 +1,29 @@
+package software.ujithamigara.orm_concepts_course_work.dao.custom.impl;
+
+import software.ujithamigara.orm_concepts_course_work.dao.custom.ReservationDAO;
+import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ReservationDAOImpl implements ReservationDAO {
+    @Override
+    public boolean save(Reservation reservation) throws SQLException, IOException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, IOException {
+        return false;
+    }
+
+    @Override
+    public Reservation search(String id) throws SQLException, IOException {
+        return null;
+    }
+
+    @Override
+    public boolean update(Reservation reservation) throws SQLException, IOException {
+        return false;
+    }
+}
