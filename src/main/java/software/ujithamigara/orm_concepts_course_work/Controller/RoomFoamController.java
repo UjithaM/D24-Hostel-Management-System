4a3698f3ed74e15bb84d06fcf19cc09c028f7be3
uji100 <ujithamigara@gmail.com>
2023-09-06 20:37:39 +0530

    add Student.fxml and StudentController

---
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/RoomFoamController.java
@@ -0,0 +1,36 @@
+package software.ujithamigara.orm_concepts_course_work.Controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RoomFoamController {
+
+    @FXML
+    private JFXTextField cityTextField;
+
+    @FXML
+    private JFXTextField keyMoneyTextField;
+
+    @FXML
+    private JFXTextField roomIdTextField;
+
+    @FXML
+    private JFXComboBox<?> roomTypeComboBOx;
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+}
