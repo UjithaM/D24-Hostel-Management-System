a4aee7e3dd27becb284a799513f80fb3df4f93a5
uji100 <ujithamigara@gmail.com>
2023-09-06 21:54:14 +0530

    update with components

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/StudentController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/StudentController.java
@@ -1,4 +1,46 @@
 package software.ujithamigara.orm_concepts_course_work.Controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
 public class StudentController {
+
+    @FXML
+    private ToggleGroup Gender;
+
+    @FXML
+    private ToggleGroup GenderRadioButton;
+
+    @FXML
+    private JFXTextField dateOfBirthTextField;
+
+    @FXML
+    private JFXTextField studentAddressTextField;
+
+    @FXML
+    private JFXTextField studentContactTextField;
+
+    @FXML
+    private JFXTextField studentIDTextField;
+
+    @FXML
+    private JFXTextField studentNameTextField;
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+
 }
