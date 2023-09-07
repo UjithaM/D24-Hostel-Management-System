ff4f6a619106d7d581321630571662edbfae1cfb
uji100 <ujithamigara@gmail.com>
2023-09-07 20:46:37 +0530

    develop userDAO and userDAOImpl

--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/UserDAO.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/UserDAO.java
@@ -2,7 +2,9 @@ package software.ujithamigara.orm_concepts_course_work.dao.custom;
 
 import software.ujithamigara.orm_concepts_course_work.entity.User;
 
+import java.io.IOException;
+
 public interface UserDAO {
-    boolean save(User user);
-    boolean update(User user);
+    boolean save(User user) throws IOException;
+    boolean update(User user) throws IOException;
 }
--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/UserDAOImpl.java
@@ -1,16 +1,45 @@
 package software.ujithamigara.orm_concepts_course_work.dao.custom.impl;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import software.ujithamigara.orm_concepts_course_work.dao.custom.UserDAO;
 import software.ujithamigara.orm_concepts_course_work.entity.User;
+import software.ujithamigara.orm_concepts_course_work.util.FactoryConfiguration;
+
+import java.io.IOException;
 
 public class UserDAOImpl implements UserDAO {
     @Override
-    public boolean save(User user) {
-        return false;
+    public boolean save(User user) throws IOException {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(user);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
-    public boolean update(User user) {
-        return false;
+    public boolean update(User user) throws IOException {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.update(user);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
     }
 }
