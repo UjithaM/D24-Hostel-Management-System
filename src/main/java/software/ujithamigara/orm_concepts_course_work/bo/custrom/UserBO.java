1403d5e6bd66246e54dd59dfd0fc8f38c6c9478b
uji100 <ujithamigara@gmail.com>
2023-09-07 21:04:42 +0530

    update throw exception

--- src/main/java/software/ujithamigara/orm_concepts_course_work/bo/custrom/UserBO.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/bo/custrom/UserBO.java
@@ -2,7 +2,9 @@ package software.ujithamigara.orm_concepts_course_work.bo.custrom;
 
 import software.ujithamigara.orm_concepts_course_work.dto.UserDTO;
 
+import java.io.IOException;
+
 public interface UserBO {
-    boolean saveUser(UserDTO userDTO);
-    boolean updateUser(UserDTO userDTO);
+    boolean saveUser(UserDTO userDTO) throws IOException;
+    boolean updateUser(UserDTO userDTO) throws IOException;
 }
