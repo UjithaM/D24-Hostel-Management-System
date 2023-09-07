875e67b1abaac8334c1264357ae5d4f81975cfb9
uji100 <ujithamigara@gmail.com>
2023-09-07 13:54:32 +0530

    update forget password with components

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/ForgetPasswordFormController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/ForgetPasswordFormController.java
@@ -1,4 +1,25 @@
 package software.ujithamigara.orm_concepts_course_work.Controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class ForgetPasswordFormController {
+
+    @FXML
+    private JFXTextField conformPasswordTextField;
+
+    @FXML
+    private JFXTextField newPasswordTextField;
+
+    @FXML
+    private JFXTextField oldPasswordTextField;
+
+    @FXML
+    private JFXTextField userNameTextField;
+
+    @FXML
+    void resetPasswordOnAction(ActionEvent event) {
+
+    }
 }
