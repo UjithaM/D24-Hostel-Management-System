2cb8e240ddc5c0ca6795234c01852270a022eca2
uji100 <ujithamigara@gmail.com>
2023-09-07 13:02:05 +0530

    update ReservationFoam with components

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/ReservationFoamController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/ReservationFoamController.java
@@ -1,4 +1,41 @@
 package software.ujithamigara.orm_concepts_course_work.Controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ToggleGroup;
+
 public class ReservationFoamController {
+    @FXML
+    private ToggleGroup PaidStatus;
+
+    @FXML
+    private DatePicker ReservationDatePicker;
+
+    @FXML
+    private JFXTextField ReservationIdTextField;
+
+    @FXML
+    private JFXComboBox<?> roomIdComboBox;
+
+    @FXML
+    private JFXComboBox<?> studentIdComboBOx;
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+
 }
