6e98c875e86f8fe8b641bd22cdf79f6db4ed4639
uji100 <ujithamigara@gmail.com>
2023-09-07 21:05:05 +0530

    update with DAO implement

--- src/main/java/software/ujithamigara/orm_concepts_course_work/bo/custrom/impl/UserBOImpl.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/bo/custrom/impl/UserBOImpl.java
@@ -1,16 +1,22 @@
 package software.ujithamigara.orm_concepts_course_work.bo.custrom.impl;
 
 import software.ujithamigara.orm_concepts_course_work.bo.custrom.UserBO;
+import software.ujithamigara.orm_concepts_course_work.dao.DAOFactory;
+import software.ujithamigara.orm_concepts_course_work.dao.custom.UserDAO;
 import software.ujithamigara.orm_concepts_course_work.dto.UserDTO;
+import software.ujithamigara.orm_concepts_course_work.entity.User;
+
+import java.io.IOException;
 
 public class UserBOImpl implements UserBO {
+    UserDAO userDAO = (UserDAO) DAOFactory.getInstance().getDao(DAOFactory.DaoTypes.UserDao);
     @Override
-    public boolean saveUser(UserDTO userDTO) {
-        return false;
+    public boolean saveUser(UserDTO userDTO) throws IOException {
+        return userDAO.save(new User(userDTO.getUserId(), userDTO.getName(), userDTO.getPassword()));
     }
 
     @Override
-    public boolean updateUser(UserDTO userDTO) {
-        return false;
+    public boolean updateUser(UserDTO userDTO) throws IOException {
+        return userDAO.update(new User(userDTO.getUserId(), userDTO.getName(), userDTO.getPassword()));
     }
 }
