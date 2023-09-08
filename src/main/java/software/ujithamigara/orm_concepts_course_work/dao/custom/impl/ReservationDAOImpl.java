a4512fa4737e343c16c16e706a9e902d85ecce99
uji100 <ujithamigara@gmail.com>
2023-09-08 14:10:58 +0530

    update add get alla

--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/ReservationDAOImpl.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/ReservationDAOImpl.java
@@ -5,6 +5,7 @@ import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class ReservationDAOImpl implements ReservationDAO {
     @Override
@@ -25,5 +26,10 @@ public class ReservationDAOImpl implements ReservationDAO {
     @Override
     public boolean update(Reservation reservation) throws SQLException, IOException {
         return false;
+    }
+
+    @Override
+    public List<Reservation> getAll() throws SQLException, IOException {
+        return null;
     }
 }
