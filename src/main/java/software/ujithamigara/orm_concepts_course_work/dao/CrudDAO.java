dec3d7981dbff7aa5035daa3ab92c9678ccd3549
uji100 <ujithamigara@gmail.com>
2023-09-08 14:06:43 +0530

    update add get all

--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/CrudDAO.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/CrudDAO.java
@@ -1,11 +1,15 @@
 package software.ujithamigara.orm_concepts_course_work.dao;
 
+import software.ujithamigara.orm_concepts_course_work.entity.Room;
+
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface CrudDAO<entity> extends SuperDAO{
     boolean save(entity entity) throws SQLException, IOException;
     boolean delete(String id) throws SQLException, IOException;
     entity search(String id) throws SQLException, IOException ;
     boolean update(entity entity) throws SQLException, IOException ;
+    public List<entity> getAllRooms() throws SQLException, IOException;
 }
