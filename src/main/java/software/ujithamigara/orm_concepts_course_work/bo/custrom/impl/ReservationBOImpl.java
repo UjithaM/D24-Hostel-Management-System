4e5f7500702183042287e28d8c86d5b019559aff
uji100 <ujithamigara@gmail.com>
2023-09-10 01:15:43 +0530

    update add unpaidReservations

--- src/main/java/software/ujithamigara/orm_concepts_course_work/bo/custrom/impl/ReservationBOImpl.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/bo/custrom/impl/ReservationBOImpl.java
@@ -2,6 +2,7 @@ package software.ujithamigara.orm_concepts_course_work.bo.custrom.impl;
 
 import software.ujithamigara.orm_concepts_course_work.bo.custrom.ReservationBO;
 import software.ujithamigara.orm_concepts_course_work.dao.DAOFactory;
+import software.ujithamigara.orm_concepts_course_work.dao.custom.QueryDAO;
 import software.ujithamigara.orm_concepts_course_work.dao.custom.ReservationDAO;
 import software.ujithamigara.orm_concepts_course_work.dto.ReservationDTO;
 import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
@@ -13,6 +14,7 @@ import java.util.List;
 
 public class ReservationBOImpl implements ReservationBO {
     ReservationDAO reservationDAO = (ReservationDAO) DAOFactory.getInstance().getDao(DAOFactory.DaoTypes.ReservationDao);
+    QueryDAO queryDAO = (QueryDAO) DAOFactory.getInstance().getDao(DAOFactory.DaoTypes.QueryDao);
     @Override
     public boolean saveReservation(ReservationDTO reservationDTO) throws SQLException, IOException {
         return reservationDAO.save(new Reservation(reservationDTO.getReservationId(), reservationDTO.getDate(), reservationDTO.getStatus(), reservationDTO.getRoom(), reservationDTO.getStudent()));
@@ -40,6 +42,16 @@ public class ReservationBOImpl implements ReservationBO {
         List<ReservationDTO> reservationDTOS = new ArrayList<>();
         for (Reservation reservation:reservations) {
             reservationDTOS.add(new ReservationDTO(reservation.getReservationId(), reservation.getDate(), reservation.getStatus(),reservation.getStudent(), reservation.getRoom()));
+        }
+        return reservationDTOS;
+    }
+
+    @Override
+    public List<ReservationDTO> findAllUnpaidReservations() throws SQLException, IOException {
+        List<Reservation> reservations= queryDAO.findAllUnpaidReservations();
+        List<ReservationDTO> reservationDTOS = new ArrayList<>();
+        for (Reservation reservation:reservations) {
+            reservationDTOS.add(new ReservationDTO(reservation.getReservationId(), reservation.getDate(), reservation.getStatus(), reservation.getStudent(), reservation.getRoom()));
         }
         return reservationDTOS;
     }
