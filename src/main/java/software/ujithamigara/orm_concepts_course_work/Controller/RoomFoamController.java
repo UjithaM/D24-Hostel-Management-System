92f259426f28597a0c9d97bf54ec39e768e415d6
uji100 <ujithamigara@gmail.com>
2023-09-09 20:44:25 +0530

    update with validations

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/RoomFoamController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/RoomFoamController.java
@@ -10,11 +10,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import software.ujithamigara.orm_concepts_course_work.bo.BOFactory;
 import software.ujithamigara.orm_concepts_course_work.bo.custrom.RoomBO;
 import software.ujithamigara.orm_concepts_course_work.dto.RoomDTO;
 import software.ujithamigara.orm_concepts_course_work.dto.tm.RoomTM;
+import software.ujithamigara.orm_concepts_course_work.util.regex.RegExPatterns;
 
 import java.util.List;
 
@@ -75,27 +77,35 @@ public class RoomFoamController {
 
     @FXML
     void saveButtonAction(ActionEvent event) {
-        try {
-            roomBO.saveRoom(new RoomDTO(roomIdTextField.getText(), roomTypeTextField.getText(), Double.valueOf(keyMoneyTextField.getText()), Integer.valueOf(QuantityTextField.getText())));
-            new Alert(Alert.AlertType.CONFIRMATION, "Room saved successfully ! ").show();
-            clearTextField();
-            refreshTable();
-        } catch (Exception e) {
-            new Alert(Alert.AlertType.ERROR, "Room not saved ! ").show();
-            e.printStackTrace();
+        if (validation()) {
+            try {
+                roomBO.saveRoom(new RoomDTO(roomIdTextField.getText(), roomTypeTextField.getText(), Double.valueOf(keyMoneyTextField.getText()), Integer.valueOf(QuantityTextField.getText())));
+                new Alert(Alert.AlertType.CONFIRMATION, "Room saved successfully ! ").show();
+                clearTextField();
+                refreshTable();
+            } catch (Exception e) {
+                new Alert(Alert.AlertType.ERROR, "Room not saved ! ").show();
+                e.printStackTrace();
+            }
+        }else {
+            new Alert(Alert.AlertType.ERROR, "invalid input data in fields!").show();
         }
     }
 
     @FXML
     void updateOnAction(ActionEvent event) {
-        try {
-            roomBO.updateRoom(new RoomDTO(roomIdTextField.getText(), roomTypeTextField.getText(), Double.valueOf(keyMoneyTextField.getText()), Integer.valueOf(QuantityTextField.getText())));
-            new Alert(Alert.AlertType.CONFIRMATION, "Room updated successfully ! ").show();
-            clearTextField();
-            refreshTable();
-        } catch (Exception e) {
-            new Alert(Alert.AlertType.ERROR, "Room Not Updated ! ").show();
-            e.printStackTrace();
+        if (validation()) {
+            try {
+                roomBO.updateRoom(new RoomDTO(roomIdTextField.getText(), roomTypeTextField.getText(), Double.valueOf(keyMoneyTextField.getText()), Integer.valueOf(QuantityTextField.getText())));
+                new Alert(Alert.AlertType.CONFIRMATION, "Room updated successfully ! ").show();
+                clearTextField();
+                refreshTable();
+            } catch (Exception e) {
+                new Alert(Alert.AlertType.ERROR, "Room Not Updated ! ").show();
+                e.printStackTrace();
+            }
+        }else {
+            new Alert(Alert.AlertType.ERROR, "invalid input data in fields!").show();
         }
     }
 
@@ -130,5 +140,43 @@ public class RoomFoamController {
         } catch (RuntimeException e) {
             new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
+    }
+
+    public void roomIdOnKeyReleased(KeyEvent keyEvent) {
+        if (!roomIdTextField.getText().matches(RegExPatterns.getRoomIdPattern().pattern())){
+            roomIdTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            roomIdTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    public void roomTypeOnKeyReleased(KeyEvent keyEvent) {
+        if (!roomTypeTextField.getText().matches(RegExPatterns.getAddressPattern().pattern())){
+            roomTypeTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            roomTypeTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    public void keyMoneyOnKeyReleased(KeyEvent keyEvent) {
+        if (!keyMoneyTextField.getText().matches(RegExPatterns.getIntPattern().pattern())){
+            keyMoneyTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            keyMoneyTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    public void quantityOnKeyReleased(KeyEvent keyEvent) {
+        if (!QuantityTextField.getText().matches(RegExPatterns.getIntPattern().pattern())){
+            QuantityTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            QuantityTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+    private boolean validation() {
+        return roomIdTextField.getText().matches(RegExPatterns.getRoomIdPattern().pattern())
+                && roomTypeTextField.getText().matches(RegExPatterns.getAddressPattern().pattern())
+                && keyMoneyTextField.getText().matches(RegExPatterns.getIntPattern().pattern())
+                && QuantityTextField.getText().matches(RegExPatterns.getIntPattern().pattern());
     }
 }
