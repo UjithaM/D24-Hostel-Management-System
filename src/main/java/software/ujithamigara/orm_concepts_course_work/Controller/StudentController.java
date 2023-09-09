b0674979664864fbb2cd1347e6bd2477ebbeb8f0
uji100 <ujithamigara@gmail.com>
2023-09-09 20:11:42 +0530

    update setup all validation in StudentController

--- src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/StudentController.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/Controller/StudentController.java
@@ -8,11 +8,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import software.ujithamigara.orm_concepts_course_work.bo.BOFactory;
 import software.ujithamigara.orm_concepts_course_work.bo.custrom.StudentBO;
 import software.ujithamigara.orm_concepts_course_work.dto.StudentDTO;
 import software.ujithamigara.orm_concepts_course_work.dto.tm.StudentTM;
+import software.ujithamigara.orm_concepts_course_work.util.regex.RegExPatterns;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -98,39 +100,47 @@ public class StudentController {
 
     @FXML
     void saveButtonAction(ActionEvent event) {
-        try {
-            RadioButton selectedRadioButton = (RadioButton) GenderRadioButton.getSelectedToggle();
-            if(studentBO.saveStudent(new StudentDTO(studentIDTextField.getText(), studentNameTextField.getText(),
-                    studentAddressTextField.getText(), studentContactTextField.getText(),
-                    dateOfBirthDatePicker.getValue(), selectedRadioButton.getText()))){
-                new Alert(Alert.AlertType.CONFIRMATION, "Student saved successfully ! ").show();
-                clear();
-                refreshTable();
-            }else {
+        if (validation()) {
+            try {
+                RadioButton selectedRadioButton = (RadioButton) GenderRadioButton.getSelectedToggle();
+                if (studentBO.saveStudent(new StudentDTO(studentIDTextField.getText(), studentNameTextField.getText(),
+                        studentAddressTextField.getText(), studentContactTextField.getText(),
+                        dateOfBirthDatePicker.getValue(), selectedRadioButton.getText()))) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Student saved successfully ! ").show();
+                    clear();
+                    refreshTable();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Student not saved ! ").show();
+                }
+            } catch (Exception e) {
                 new Alert(Alert.AlertType.ERROR, "Student not saved ! ").show();
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            new Alert(Alert.AlertType.ERROR, "Student not saved ! ").show();
-            e.printStackTrace();
+        }else {
+            new Alert(Alert.AlertType.ERROR, "invalid input data in fields!").show();
         }
     }
 
     @FXML
     void updateOnAction(ActionEvent event) {
-        try {
-            RadioButton selectedRadioButton = (RadioButton) GenderRadioButton.getSelectedToggle();
-            if(studentBO.updateStudent(new StudentDTO(studentIDTextField.getText(), studentNameTextField.getText(),
-                    studentAddressTextField.getText(), studentContactTextField.getText(),
-                    dateOfBirthDatePicker.getValue(), selectedRadioButton.getText()))){
-                new Alert(Alert.AlertType.CONFIRMATION, "Student Update successfully ! ").show();
-                clear();
-                refreshTable();
-            }else {
+        if (validation()) {
+            try {
+                RadioButton selectedRadioButton = (RadioButton) GenderRadioButton.getSelectedToggle();
+                if (studentBO.updateStudent(new StudentDTO(studentIDTextField.getText(), studentNameTextField.getText(),
+                        studentAddressTextField.getText(), studentContactTextField.getText(),
+                        dateOfBirthDatePicker.getValue(), selectedRadioButton.getText()))) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Student Update successfully ! ").show();
+                    clear();
+                    refreshTable();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Student not Updated ! ").show();
+                }
+            } catch (Exception e) {
                 new Alert(Alert.AlertType.ERROR, "Student not Updated ! ").show();
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            new Alert(Alert.AlertType.ERROR, "Student not Updated ! ").show();
-            e.printStackTrace();
+        }else {
+            new Alert(Alert.AlertType.ERROR, "invalid input data in fields!").show();
         }
     }
     void clear(){
@@ -172,5 +182,46 @@ public class StudentController {
             new Alert(Alert.AlertType.ERROR, exception.getMessage()).show();
             studentTable.getItems().clear();
         }
+    }
+
+    public void studentIdOnKeyReleased(KeyEvent keyEvent) {
+        if ((!studentIDTextField.getText().matches(RegExPatterns.getStudentIdPattern().pattern()))){
+            studentIDTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            studentIDTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    public void studentNameOnKeyReleased(KeyEvent keyEvent) {
+        if (!studentNameTextField.getText().matches(RegExPatterns.getNamePattern().pattern())){
+            studentNameTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            studentNameTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    public void studentContactOnKeyReleased(KeyEvent keyEvent) {
+        if (!studentContactTextField.getText().matches(RegExPatterns.getMobilePattern().pattern())){
+            studentContactTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            studentContactTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    public void studentAddressOnKeyReleased(KeyEvent keyEvent) {
+        if (!studentAddressTextField.getText().matches(RegExPatterns.getAddressPattern().pattern())){
+            studentAddressTextField.setStyle("-jfx-unfocus-color: red;");
+        }else {
+            studentAddressTextField.setStyle("-jfx-unfocus-color: #4059a9;");
+        }
+    }
+
+    private boolean validation() {
+        return studentIDTextField.getText().matches(RegExPatterns.getStudentIdPattern().pattern())
+                && studentNameTextField.getText().matches(RegExPatterns.getNamePattern().pattern())
+                && studentContactTextField.getText().matches(RegExPatterns.getMobilePattern().pattern())
+                && studentAddressTextField.getText().matches(RegExPatterns.getAddressPattern().pattern())
+                && dateOfBirthDatePicker.getValue() != null
+                && GenderRadioButton.getSelectedToggle() != null;
     }
 }
