74fd0e4a34113884aa19e5c74443edb4d2094f4c
uji100 <ujithamigara@gmail.com>
2023-09-10 01:14:43 +0530

    update implement get unPaid reservation

--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/QueryDAO.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/QueryDAO.java
@@ -1,6 +1,13 @@
 package software.ujithamigara.orm_concepts_course_work.dao.custom;
 
+
 import software.ujithamigara.orm_concepts_course_work.dao.SuperDAO;
+import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
+import software.ujithamigara.orm_concepts_course_work.entity.Student;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface QueryDAO extends SuperDAO {
+    public List<Reservation> findAllUnpaidReservations() throws IOException;
 }
--- src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/QueryDAOImpl.java
+++ src/main/java/software/ujithamigara/orm_concepts_course_work/dao/custom/impl/QueryDAOImpl.java
@@ -1,6 +1,36 @@
 package software.ujithamigara.orm_concepts_course_work.dao.custom.impl;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import software.ujithamigara.orm_concepts_course_work.dao.custom.QueryDAO;
+import software.ujithamigara.orm_concepts_course_work.entity.Reservation;
+import software.ujithamigara.orm_concepts_course_work.entity.Student;
+import software.ujithamigara.orm_concepts_course_work.util.FactoryConfiguration;
+
+import java.io.IOException;
+import java.util.List;
 
 public class QueryDAOImpl implements QueryDAO {
+    @Override
+    public List<Reservation> findAllUnpaidReservations() throws IOException {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        List<Reservation> reservations = null;
+
+        try {
+            String hql = "FROM Reservation r WHERE r.status = 'Not Paid'";
+            Query<Reservation> query = session.createQuery(hql, Reservation.class);
+
+            reservations = query.list();
+            transaction.commit();
+            return reservations;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return null;
+    }
 }
